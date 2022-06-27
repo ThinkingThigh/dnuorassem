a1feccb92f171357b1ceb3f0929a2e4e833b126a
thinkingthigh <thinkingthigh@gmail.com>
2022-06-27 18:05:01 +0800

    feat: 修改模板

--- template/template.js
+++ template/template.js
@@ -1,281 +1,162 @@
-const template  = `
+const template = `
 // {0}
 // {1}
-const vscode = require('vscode');
+/* Base64 加密解密*/
 
-// {2}
-// {3}
-
-/**
- * @param {vscode.ExtensionContext} {4}
- */
-function activate(context) {
-
+function Base64() {  
+   
+    // private property  
+	// {3}
+    _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";  
+   
+    // public method for encoding  
+	// {4}
 	// {5}
-	// {6}
-	console.log('Congratulations, your extension "codenovel" is now active!');
-
-	// {7}
-	//{8}
-	//{9}
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// {10}
-
+    this.encode = function (input) {  
+        var output = "";  
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;  
+        var i = 0;  
+        input = _utf8_encode(input);  
+		// {6}
+        while (i < input.length) {  
+            chr1 = input.charCodeAt(i++);  
+            chr2 = input.charCodeAt(i++);  
+            chr3 = input.charCodeAt(i++);  
+            enc1 = chr1 >> 2;  
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);  
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);  
+            enc4 = chr3 & 63;  
+			// {7}
+			//{8}
+			//{9}
+            if (isNaN(chr2)) {  
+                enc3 = enc4 = 64;  
+            } else if (isNaN(chr3)) {  
+                enc4 = 64;  
+            }  
+			// {10}
 		// {11}
-		vscode.window.showInformationMessage('Hello World!');
-	});
+            output = output +  
+            _keyStr.charAt(enc1) + _keyStr.charAt(enc2) +  
+            _keyStr.charAt(enc3) + _keyStr.charAt(enc4);  
+        }  
+		// {12}
+        return output;  
+    }  
+   
+    // public method for decoding  
+	// {13}
+	/**
+	 * @param {vscode.ExtensionContext} {14}
+	 */
+    this.decode = function (input) {  
 
-	context.subscriptions.push(disposable);
+		// {15}
+		// {16}
+        var output = "";  
+        var chr1, chr2, chr3;  
+        var enc1, enc2, enc3, enc4;  
+        var i = 0;  
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");  
+		// {17}
+		// {18}
+		// {19}
+        while (i < input.length) {  
+            enc1 = _keyStr.indexOf(input.charAt(i++));  
+            enc2 = _keyStr.indexOf(input.charAt(i++));  
+            enc3 = _keyStr.indexOf(input.charAt(i++));  
+            enc4 = _keyStr.indexOf(input.charAt(i++));  
+			// {20}
+			// {21}
+            chr1 = (enc1 << 2) | (enc2 >> 4);  
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);  
+            chr3 = ((enc3 & 3) << 6) | enc4;  
+            output = output + String.fromCharCode(chr1);  
+			// {22}
+            if (enc3 != 64) {  
+                output = output + String.fromCharCode(chr2);  // {23}
+            }  
+			// {24}
+
+            if (enc4 != 64) {  
+				// {25}
+				// {26}
+                output = output + String.fromCharCode(chr3);  
+            }  
+        }  
+
+		// {27}
+		// {28}
+		
+        output = _utf8_decode(output);  
+        return output;   // {29}
+    }  
+   
+    // private method for UTF-8 encoding  
+	// {30}
+    _utf8_encode = function (string) {  
+        string = string.replace(/\r\n/g,"\n");  
+        var utftext = "";  
+		// {31}
+		// {32}
+        for (var n = 0; n < string.length; n++) {  
+            var c = string.charCodeAt(n);  
+            if (c < 128) {  
+                utftext += String.fromCharCode(c);  // {33}
+            } else if((c > 127) && (c < 2048)) {  
+				// {34}
+                utftext += String.fromCharCode((c >> 6) | 192);  
+                utftext += String.fromCharCode((c & 63) | 128);  
+            } else {  
+				// {35}
+                utftext += String.fromCharCode((c >> 12) | 224);  
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);  
+                utftext += String.fromCharCode((c & 63) | 128);  
+            }  
+
+			// {36}
+			// {37}
+   
+        }  
+        return utftext;  
+    }  
+   
+    // private method for UTF-8 decoding  
+    _utf8_decode = function (utftext) {  
+		// {38}
+		//{39}
+		//{40}
+        var string = "";  
+        var i = 0;  
+        var c = c1 = c2 = 0;  
+        while ( i < utftext.length ) {  
+            c = utftext.charCodeAt(i);  
+            if (c < 128) {  
+                string += String.fromCharCode(c);  // {41}
+                i++;  
+            } else if((c > 191) && (c < 224)) {  
+                c2 = utftext.charCodeAt(i+1);  // {42}
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));  
+                i += 2;  
+            } else {  
+                c2 = utftext.charCodeAt(i+1);  // {43}
+                c3 = utftext.charCodeAt(i+2);  
+
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));  // {44}
+				// {45}
+                i += 3;  
+            }  
+			// {46}
+			// {47}
+        }  
+
+		// {48}
+		// {49}
+		
+        return string;  // {50}
+    }  
 }
-exports.activate = activate;
-
-// {12}
-function deactivate() {}
-
-// {13}
-
-/**
- * @param {vscode.ExtensionContext} {14}
- */
-function activate(context) {
-
-	// {15}
-	// {16}
-	console.log('Congratulations, your extension "codenovel" is now active!');
-
-	// {17}
-	// {18}
-	// {19}
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// {20}
-
-		// {21}
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
-}
-exports.activate = activate;
-
-// {22}
-function deactivate() {}
-
-// {23}
-
-/**
- * @param {vscode.ExtensionContext} {24}
- */
-function activate(context) {
-
-	// {25}
-	// {26}
-	console.log('Congratulations, your extension "codenovel" is now active!');
-
-	// {27}
-	// {28}
-	// {29}
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// {30}
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
-}
-exports.activate = activate;
-function deactivate() {}
-// {31}
-// {32}
-const vscode = require('vscode');
-
-// {33}
-// {34}
-
-/**
- * @param {vscode.ExtensionContext} {35}
- */
-function activate(context) {
-
-	// {36}
-	// {37}
-	console.log('Congratulations, your extension "codenovel" is now active!');
-
-	// {38}
-	//{39}
-	//{40}
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// {41}
-
-		// {42}
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
-}
-exports.activate = activate;
-
-// {43}
-function deactivate() {}
-
-// {44}
-
-/**
- * @param {vscode.ExtensionContext} {45}
- */
-function activate(context) {
-
-	// {46}
-	// {47}
-	console.log('Congratulations, your extension "codenovel" is now active!');
-
-	// {48}
-	// {49}
-	// {50}
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// {51}
-
-		// {52}
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
-}
-exports.activate = activate;
-
-// {53}
-function deactivate() {}
-
-// {54}
-
-/**
- * @param {vscode.ExtensionContext} {55}
- */
-function activate(context) {
-
-	// {56}
-	// {57}
-	console.log('Congratulations, your extension "codenovel" is now active!');
-
-	// {58}
-	// {59}
-	// {60}
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// {61}
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
-}
-exports.activate = activate;
-function deactivate() {}
-
-module.exports = {
-	activate,
-	deactivate
-}
-// {62}
-// {63}
-const vscode = require('vscode');
-
-// {64}
-// {65}
-
-/**
- * @param {vscode.ExtensionContext} {66}
- */
-function activate(context) {
-
-	// {67}
-	// {68}
-	console.log('Congratulations, your extension "codenovel" is now active!');
-
-	// {69}
-	//{70}
-	//{71}
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// {72}
-
-		// {73}
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
-}
-exports.activate = activate;
-
-// {74}
-function deactivate() {}
-
-// {75}
-
-/**
- * @param {vscode.ExtensionContext} {76}
- */
-function activate(context) {
-
-	// {77}
-	// {78}
-	console.log('Congratulations, your extension "codenovel" is now active!');
-
-	// {79}
-	// {80}
-	// {81}
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// {82}
-
-		// {83}
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
-}
-exports.activate = activate;
-
-// {84}
-function deactivate() {}
-
-// {85}
-
-/**
- * @param {vscode.ExtensionContext} {86}
- */
-function activate(context) {
-
-	// {87}
-	// {88}
-	console.log('Congratulations, your extension "codenovel" is now active!');
-
-	// {89}
-	// {90}
-	// {91}
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// {92}
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
-}
-/**
- * @param {vscode.ExtensionContext} {93}
- */
-function activate(context) {
-
-	// {94}
-	// {95}
-	console.log('Congratulations, your extension "codenovel" is now active!');
-
-	// {96}
-	// {97}
-	// {98}
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// {99}
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
-}
-exports.activate = activate;
-function deactivate() {}
 
 
-`
+`;
 module.exports = template;
